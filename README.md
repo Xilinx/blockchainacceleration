e8b5bb5ef732a90df6b3516b83aa6639e47f3804
Tuo Lin <tuo.lin@xilinx.com>
2021-07-09 07:28:01 +0800

    Update build.sh and readme (#3)
    * update build.sh and add patch
    
    * fix boost package download error issue
    
    * update README.md
    
    Co-authored-by: Tuo Lin <tuol@xilinx.com>

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # EthereumMiner
 
+EthereumMiner is Xilinx's open-sourced solution written in C++ for Ethereum mining. 
+
+Currently, this solution offers:
+* On device side, a Vitis based kernel design to accelerate Ethash, which is hashing algorithm of Ethereum.
+* Also, a Vitis based kernel design to accelerate "DAG" generation, which provides the large dataset needed by Ethash.
+* On host side, a patch to "ethminer" which is the most popular mining software, for customer to do mining with their own account and desired mining pool.
+
+## Requirement
+* Currently this solution support Xilinx Alveo U55N, you need installed packages for the card, like XRT / firmware and development package for the card.
+* If you would like to build the xclbin files on your own machine, you'll also need Vitis that is at least 2021.1_released or later version. You could also download pre-buildt xclbin files from Xilinx. Please take reference from "Step to run". 
+
+## Step to run
+* 1. Get xclbins ready.
+  You can either download them from Xilinx or try build them yourself.
+  To download from Xilinx: "./build.sh get_xclbin".
+  To build your self: "./build.sh build_ethash" and "./build.sh build_daggen"
+* 2. Get ethminer ready: "./build.sh build_host"
+* 3. Run: "./build mine <your_account> <your_mine_name> <mining_pool_address>"
+  
+
+
 ## License
 
 Licensed using the [Apache 2.0 license](https://www.apache.org/licenses/LICENSE-2.0).
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # ## License
 
@@ -19,3 +19,157 @@
 #     limitations under the License.
 
 # This is script to download ethminer from github, make Xilinx patch to it and build.
+
+function print_help() {
+    echo ""
+    echo "Usage: ./build.sh [option]"
+    echo "Options:"
+    echo "help                                     print this help"
+    echo "build_ethash                             build ethash_kernel.xclbin"
+    echo "build_daggen                             build dag_gen_kernel.xclbin"
+    echo "build_host                               build ethminer"
+    echo "get_xclbin                               down xclbin files needed from Xilinx OMS"
+    echo "mine <wallet> <name> <pool_address>      start mining with your wallet, name and pool address"
+    echo ""
+}
+
+function build_ethash() {
+    echo ""
+    if [ ! $XILINX_XRT ]
+    then
+        echo "Please setup XRT"
+    elif [ ! $XILINX_VITIS ]
+    then
+        echo "Please setup Vitis"
+    else
+        export DEVICE=u55n_gen3x4
+        export TARGET=hw
+        echo "Start to build ethash_kernel.xclbin, will takes several hours"
+        cd ./hw/ethash8Greturn
+        make all
+        cp build_dir.$TARGET.$DEVICE/ethash_kernel.xclbin ../../
+        cd ../..
+    fi
+    echo ""
+}
+
+function build_daggen() {
+    echo ""
+    if [ ! $XILINX_XRT ]
+    then
+        echo "Please setup XRT"
+    elif [ ! $XILINX_VITIS ]
+    then
+        echo "Please setup Vitis"
+    else
+        export DEVICE=u55n_gen3x4
+        export TARGET=hw
+        echo "Start to build dag_gen_kernel.xclbin, will takes several hours"
+        cd ./hw/genDAG
+        make all
+        cp build_dir.$TARGET.$DEVICE/dag_gen_kernel.xclbin ../../
+        cd ../..
+    fi
+    echo ""
+}
+
+function get_xclbin() {
+    echo ""
+    echo "Download xclbins files needed from Xilinx OMS, not finished yet"
+    echo ""
+}
+
+function build_host() {
+    echo ""
+    if [ ! $XILINX_XRT ]
+    then
+        echo "Please setup XRT"
+    else
+        echo "Download ethminer"
+        git clone https://github.com/ethereum-mining/ethminer.git
+        https://boostorg.jfrog.io/artifactory/main/release/1.66.0/source/boost_1_66_0.7z
+        mv boost_1_66_0.7z ~/.hunter/_Base/Download/Boost/1.66.0/075d0b4/
+        cd ethminer
+        git checkout -b xilinx_platform
+        git reset --hard cd75c13d38eceb6fed78d47104440a762ca1894e
+        git am --abort
+        git am ../sw/*.patch
+        git submodule update --init --recursive
+        mkdir build
+        cd build
+        cmake ..
+        make -j 10
+        cd ../../
+    fi
+    echo ""
+}
+
+function mine() {
+    echo ""
+    echo "mine"
+    echo "account:              $1"
+    echo "name:                 $2"
+    echo "mining pool address:  $3"
+
+    xclbin_1="ethash_kernel.xclbin"
+    xclbin_2="dag_gen_kernel.xclbin"
+    exe_file="ethminer/build/ethminer/ethminer"
+    if [ -f $xclbin_1 ]
+    then
+        cp $xclbin_1 ethminer/build/ethminer/
+    else
+        echo "Please build $xclbin_1 or download from Xilinx"
+    fi
+    if [ -f $xclbin_2 ]
+    then
+        cp $xclbin_2 ethminer/build/ethminer/
+    else
+        echo "Please build $xclbin_2 or download from Xilinx"
+    fi
+    if [ -f $exe_file ]
+    then
+        :
+    else
+        echo "Please build ethminer"
+    fi
+
+    if [ -f $xclbin_1 ] && [ -f $xclbin_2 ] && [ -f $exe_file ]
+    then
+        $exe_file -P stratum1+tcp://$1.$2@$3 | tee ./test.log
+    fi
+}
+
+
+if [ $# == 0 ]
+then
+    print_help
+elif [ $1 = help ]
+then
+    print_help
+elif [ $1 = build_ethash ]
+then
+    build_ethash
+elif [ $1 = build_daggen ]
+then
+    build_daggen
+elif [ $1 = get_xclbin ]
+then
+    get_xclbin
+elif [ $1 = build_host ]
+then
+    build_host
+elif [ $1 = mine ]
+then
+    if [ $# = 4 ]
+    then
+        mine $2 $3 $4
+    else
+        echo ""
+        echo "Need provide acount, name and mining pool address"
+        print_help
+    fi
+else
+    echo ""
+    echo "Unrecognized option"
+    print_help
+fi
